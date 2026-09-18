8cdc990c48bc1bd0cbfa463283f266ea557fa9dd
cathryntanmua <cathryntan.mua@gmail.com>
2026-09-18 13:55:53 +0800

    Backfill two commission portions for existing records

--- drizzle/0004_commission_two_portions.sql
+++ drizzle/0004_commission_two_portions.sql
@@ -24,3 +24,20 @@ CREATE INDEX "commission_portion_staff_idx" ON "commission_portions" USING btree
 ALTER TABLE "commission_portions" ADD CONSTRAINT "commission_portions_collection_id_collections_id_fk" FOREIGN KEY ("collection_id") REFERENCES "collections"("id") ON DELETE CASCADE;
 --> statement-breakpoint
 ALTER TABLE "commission_portions" ADD CONSTRAINT "commission_portions_staff_id_staff_id_fk" FOREIGN KEY ("staff_id") REFERENCES "staff"("id");
+--> statement-breakpoint
+INSERT INTO "commission_portions" ("collection_id","staff_id","portion","release_month","amount_sen")
+SELECT ca.collection_id, ca.staff_id, 1, substring(c.collection_date::text,1,7),
+       round(ca.commission_amount_sen * 0.5)
+FROM "collection_allocations" ca
+JOIN "collections" c ON c.id = ca.collection_id
+WHERE ca.commission_amount_sen > 0
+ON CONFLICT ("collection_id","staff_id","portion") DO NOTHING;
+--> statement-breakpoint
+INSERT INTO "commission_portions" ("collection_id","staff_id","portion","release_month","amount_sen")
+SELECT ca.collection_id, ca.staff_id, 2,
+       substring(coalesce(c.wedding_pickup_date, c.collection_date)::text,1,7),
+       ca.commission_amount_sen - round(ca.commission_amount_sen * 0.5)
+FROM "collection_allocations" ca
+JOIN "collections" c ON c.id = ca.collection_id
+WHERE ca.commission_amount_sen > 0
+ON CONFLICT ("collection_id","staff_id","portion") DO NOTHING;
